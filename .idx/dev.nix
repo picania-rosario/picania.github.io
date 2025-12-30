7353096c9defd693eb6ffe7fec8be46504cb7933
picaniarosario <picaniarosario@gmail.com>
2025-12-30 18:21:27 +0000

    dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,13 +5,10 @@
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
-  packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    packages = [
+    pkgs.nodejs_20
   ];
+
 
   # Sets environment variables in the workspace
   env = {};
