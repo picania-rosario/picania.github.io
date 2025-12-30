c48ddb2e887646e5af41fbbc7fd406cd485eba0a
picaniarosario <picaniarosario@gmail.com>
2025-12-30 18:49:34 +0000

    Revert "dev.nix"
    This reverts commit 7353096c9defd693eb6ffe7fec8be46504cb7933.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,10 +5,13 @@
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
-    packages = [
-    pkgs.nodejs_20
+  packages = [
+    # pkgs.go
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
   ];
-
 
   # Sets environment variables in the workspace
   env = {};
